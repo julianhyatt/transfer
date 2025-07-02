5def5180f5b77d3c5d46f5830ee2d9f5d8ddc807
Julian Hyatt <54834203+julianhyatt@users.noreply.github.com>
2025-07-02 08:07:52 +0200

    Update JellyfishTransferExtension.php

--- src/DependencyInjection/JellyfishTransferExtension.php
+++ src/DependencyInjection/JellyfishTransferExtension.php
@@ -17,10 +17,15 @@ class JellyfishTransferExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
         $loader = new XmlFileLoader(
             $container,
             new FileLocator(__DIR__ . '/../Resources/config'),
         );
         $loader->load('services.xml');
+
+        $container->setParameter('jellyfish_transfer.namespace_prefix', $config['namespace_prefix']);
     }
 }
